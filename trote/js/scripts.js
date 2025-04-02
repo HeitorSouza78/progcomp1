72448913522837dc665b86f2daaf3713bc578dcb
Heitor Silvério de Souza <aleribasouza@gmail.com>
2025-04-01 22:27:04 -0300

    if else

--- trote/js/scripts.js
+++ trote/js/scripts.js
@@ -1,15 +1,36 @@
 function calcular(){
     // vamos criar 2 variáveis
     // JS as variáveis não possuem tipo
-    let mascote, homenagem
+    let mascote, homenagem, leite, kit, suplemento, soma, equipe
     // recupera o valor do mascote digitanddo pelo usuário
-    mascote = document.getElementById("mascote").value
-    alert(mascote)
+    mascote = Number(document.getElementById("mascote").value)
     //recupera o valor da homenagem digitada pelo usuário
-    homenagem = document.getElementById("homenagem").value
-    alert(homenagem)
-    // calcular a soma
-    let soma= Number(mascote) + Number(homenagem)
-    alert(soma)
-
+    homenagem = Number(document.getElementById("homenagem").value)
+  // calcular a soma
+  leite = Number(document.getElementById("leite").value)
+  //recupera a qtde avulsa de kits de alimentos
+  kit= Number(document.getElementById("kit").value)
+  suplemento = Number(document.getElementById("suplemento").value)
+   
+     soma=Number(mascote) + Number(homenagem) + (2 * (leite)) 
+    //vamos calcular a pontuação considerando metas de kit e supl
+     equipe = document.getElementById("equipe").value
+    if(equipe== "Laranja"){
+      if(kit >= 97) {
+        soma + soma + 5000 + ((kit- 97) * 30)
+      }
+      else if(kit>= 78){
+        soma = soma + 4000 + ((kit- 78) * 30)
+      }
+      else if(kit>=49){
+        soma = soma + 2500 + ((kit-49) * 30)
+      }
+      else if(kit >=19){
+        soma = soma + 1000 + ((kit-19) * 30)
+      }
+    }
+    // retorna o valor ao HTML
+    //template string
+     
+    document.getElementById ("soma").innerHTML = `A soma é ${soma}`
 }
